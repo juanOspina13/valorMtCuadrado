bb0dd14045bb42466c5c940df8b7c7244629e346
juanOspina13 <ospina.juan@correounivalle.edu.co>
2018-11-23 15:19:02 -0500

    grafica de regresion

--- predecirPrecioLote.py
+++ predecirPrecioLote.py
@@ -10,29 +10,49 @@ import matplotlib.animation as animation
 numero_casas = 10
 np.random.seed(42)
 tamanos_lotes = np.random.randint(low=10, high=100, size=numero_casas )
-costo_metro_cuadrado = 3
+costo_metro_cuadrado = 5
 
 #  Generamos precios de casas añadiendo un ruido aleatorio desde 1000mil hasta 8 Millones (comision )
 #np.random.seed(42)
 precios_lotes = tamanos_lotes * costo_metro_cuadrado
-print(tamanos_lotes)
-print(precios_lotes)
 # + np.random.randint(low=100000, high=8000000, size=numero_casas)  
 
 def normalizar(array):
     return (array - array.mean()) / array.std()
 
-tamano_lote_normalizado = tamanos_lotes
-precios_lotes_normalizado = precios_lotes
+def desnormalizar(array):
+    return (array + array.mean()) * array.std()
+
+def desnormalizarResultado(valor,array):
+    return (valor + (array.mean())/ array.std())
+
+# define training data
+num_train_samples = math.floor(numero_casas * 0.7)
+
+tamano_lote_entreno = np.asarray(tamanos_lotes[:num_train_samples])
+precios_lote_entreno = np.asarray(precios_lotes[:num_train_samples:])
+
+tamano_lote_entreno_normalizado = normalizar(tamano_lote_entreno)
+precio_lote_entreno_normalizado = normalizar(precios_lote_entreno)
+
+
+
+tamano_lote_pruebas = np.array(tamanos_lotes[num_train_samples:])
+precios_lote_pruebas = np.array(precios_lotes[num_train_samples:])
+
+tamano_lote_pruebas_normalizado = normalizar(tamano_lote_pruebas)
+precios_lote_pruebas_normalizado = normalizar(precios_lote_pruebas)
+
+
 
 # Pintamos la grafica de precios y tamaño
-plt.plot(tamano_lote_normalizado, precios_lotes_normalizado, "bx")
+plt.plot(tamanos_lotes, precios_lotes, "bx")
 plt.ylabel("Precio")
 plt.xlabel("Metros Cuadrados")
 plt.show()
 
 numero_datos_entreno = 10
-tf_precio_mt_cuadrado = tf.Variable( 3.,name="tf_precio_mt_cuadrado")
+tf_precio_mt_cuadrado = tf.Variable(3000.,name="tf_precio_mt_cuadrado")
 
 tf_valor_lote = tf.placeholder("float", name="tf_valor_lote")
 tf_tamanos_lote = tf.placeholder("float", name="tf_tamano_lote")
@@ -44,25 +64,56 @@ tf_costo = tf.reduce_sum(tf.pow(tf_precio_mt_cuadrado_pred-tf_valor_lote, 2))/(2
 
 # 4. Vamos a utilizar el algoritmo de gradiente descendiente
 learning_rate = 0.1
-optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(tf_costo)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(tf_costo)
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
-    iteraciones_aprendizaje = 100
+    iteraciones_aprendizaje = 200
 
     for iteration in range(iteraciones_aprendizaje):
-        for(x,y) in zip(tamano_lote_normalizado, precios_lotes_normalizado):
+        for(x,y) in zip(tamano_lote_entreno_normalizado, precio_lote_entreno_normalizado):
             optimo = sess.run(optimizer, feed_dict={tf_tamanos_lote:x,tf_valor_lote:y})
         
-        costo = sess.run(tf_costo, feed_dict={tf_tamanos_lote:tamano_lote_normalizado,tf_valor_lote:precios_lotes_normalizado})
+        costo = sess.run(tf_costo, feed_dict={tf_tamanos_lote:tamano_lote_entreno_normalizado,tf_valor_lote:precio_lote_entreno_normalizado})
         #hipotesis = sess.run(tf_precio_mt_cuadrado_pred, feed_dict={tf_tamano_lote:x})
         #print("Tamano Lote=", "{:.9f}".format(x))            
         #print("Hipotesis=", "{:.9f}".format(hipotesis))
         #print("Costo=", "{:.9f}".format(costo))
-
-    print("Valor Mt Cuadrado =>", sess.run(tf_precio_mt_cuadrado))
+    #print("Valor Mt Cuadrado =>",sess.run(tf_precio_mt_cuadrado))
  
         
             
+
+    
+    # get values used to normalized data so we can denormalize data back to its original scale
+    media_tamano_lote_entreno = tamano_lote_entreno.mean()
+    std_tamano_lote_entreno = tamano_lote_entreno.std()
+
+    media_precios_lote_entreno = precios_lote_entreno.mean()
+    std_precios_lote_entreno = precios_lote_entreno.std()
+
+    std_precios_lote = precios_lotes.std()
+    media_precios_lote = precios_lotes.mean()
+
+    # Plot the graph
+    plt.rcParams["figure.figsize"] = (10,8)
+    plt.figure()
+    plt.ylabel("Price")
+    plt.xlabel("Size (sq.ft)")
+    plt.plot(tamano_lote_entreno, precios_lote_entreno, 'go', label='Informacion Entreno')
+    plt.plot(tamano_lote_pruebas, precios_lote_pruebas, 'mo', label='Informacion pruebas')
+    plt.plot(
+            tamano_lote_entreno_normalizado *std_tamano_lote_entreno + media_tamano_lote_entreno,
+             ((sess.run(tf_precio_mt_cuadrado) * tamano_lote_entreno_normalizado ))*std_precios_lote_entreno+media_precios_lote_entreno,
+                 label='Regresion Aprendida')
+             
+    plt.legend(loc='upper left')
+    plt.show()
+    #print(precio_lote_entreno_normalizado)
+    #print(precios_lotes)    
+    #print("Valor Mt Cuadrado =>",format(sess.run(tf_precio_mt_cuadrado)* std_precios_lote_entreno+ media_precios_lote_entreno))
+    #print("***Valor en 200")
+    #print(
+        #(sess.run(tf_precio_mt_cuadrado) * normalizar( np.array( [200, 100] ) ) * std_precios_lote_entreno + media_precios_lote_entreno ) )
